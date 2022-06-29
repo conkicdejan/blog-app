2384b37b24c1b4ea8ea4b3f65059045ad44900c4
Dejan Conkic <conkicdejan@gmail.com>
2022-06-29 19:25:14 +0200

    Task 2

--- src/pages/AppPosts.jsx
+++ src/pages/AppPosts.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import PostService from '../services/PostService';
+import Post from '../components/Post';
 
 function AppPosts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = PostService.getAll();
+      if (data) {
+        setPosts(data);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>AppPosts</div>
-  )
+    <div>
+      <h4>Posts</h4>
+      {posts.length > 0 ? posts.map((post) => <Post post={post} />) : 'no posts'}
+    </div>
+  );
 }
 
-export default AppPosts
+export default AppPosts;
